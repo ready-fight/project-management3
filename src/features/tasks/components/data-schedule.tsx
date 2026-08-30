4790fdb73f2a759e9fbbace4f5ae49d29c864715
Adrian <seraphzp@gmail.com>
2026-08-30 15:24:04 +0900

    update

--- src/features/tasks/components/data-schedule.tsx
+++ src/features/tasks/components/data-schedule.tsx
@@ -5,7 +5,7 @@ import { format } from "date-fns";
 import { ja } from "date-fns/locale";
 import { PencilIcon } from "lucide-react";
 
-import { Badge } from "@/components/ui/badge";
+// import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Select,
